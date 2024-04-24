f94bd9375f18874be03dfa7125e40430c842bfab
fuxiaoqaq <lei1_ma@163.com>
2024-04-25 01:25:15 +0800

    修改侧边栏

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,11 +2,11 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-  // {
-  //   text: "工具",
-  //   icon: "tools",
-  //   prefix: "/ops/",
-  //   link: "ops/redis.md",
-  // },
-  "/ops/"
+  {
+    text: "工具",
+    icon: "tools",
+    prefix: "/ops/",
+    link: "ops/redis.md",
+  },
+  // "/ops/"
 ]);
