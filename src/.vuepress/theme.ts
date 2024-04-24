c68f63b86abafa1f03da2f542929a6b3711093c1
fuxiaoqaq <lei1_ma@163.com>
2024-04-25 01:14:00 +0800

    修改hostname

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -4,7 +4,7 @@ import sidebar from "./sidebar.js";
 // import { MR_HOPE_AVATAR } from "./logo.js";
 
 export default hopeTheme({
-  hostname: "https://mister-hope.github.io",
+  hostname: "https://fuxiaoqaq.github.io",
 
   author: {
     name: "拂晓",
